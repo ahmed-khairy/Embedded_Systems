cbddf33fb7f91e882f2d7042ab143c385c50a2aa
Ahmed khairy <52660420+ahmed-khairy@users.noreply.github.com>
2023-09-04 04:03:17 +0300

    Create star_pattern.c

---
+++ Tasks/star_pattern.c
@@ -0,0 +1,26 @@
+/*
+*Embedded systems software diploma
+*Author : Ahmed khairy
+*Date : 9/4/2023
+*Session : 
+*Program function: drawing a pattern from * each line has number of starts according to it's number
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int r,c,i;
+    printf("Enter numbers of rows : ");
+    scanf("%d",&r);
+    for(c=1;c<=r;c++)
+    {
+      for(i=1;i<=c;i++)
+      {
+          printf("*");
+      }
+      printf("\n");
+    }
+        return 0;
+
+}
