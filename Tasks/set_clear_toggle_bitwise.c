1b43697a8433a5e510cd3ed4dad1f2216077433d
Ahmed khairy <52660420+ahmed-khairy@users.noreply.github.com>
2023-09-05 01:28:07 +0300

    Update set_clear_toggle_bitwise.c

--- Tasks/set_clear_toggle_bitwise.c
+++ Tasks/set_clear_toggle_bitwise.c
@@ -1,33 +1,43 @@
 /*
-*Embedded systems software diploma
-*Author : Ahmed khairy
-*Date : 9/4/2023
-*Session : c programming > session 1 > hello c > ASG_1-2
-*Program function: how to set, clear or toggle a specific bit in any number
-*Notes : number used here is easy to follow to understand the concept
-*/
+ *Embedded systems software diploma
+ *Author : Ahmed khairy
+ *Date : 9/4/2023
+ *Session : c programming > session 1 > hello c > ASG_1-2
+ *Program function: how to set,clear or toggle a specific bit(user defined) in any number(user defined)
+ */
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    unsigned short int result1,result2;
-    unsigned short int x;
-    //set bit number 4
-    result1 = 5; //binary : 0101
-    x=1<<4; //suppose to set bit number four which makes the number : 10101 means 21
-    printf("%d\n",x);
-    result2=result1|x;
-    printf("set bit 4 in %d : %d\n",result1,result2);
-    //clear bit number 2
-    result1=7; //binary : 0111
-    x=~(1<<2); //suppose to clear bit number two which makes the number : 0011 means 3
-    result2=result1&x;
-    printf("clear bit 2 in %d : %d\n",result1,result2);
-    //toggle bit number 2
-    result1=7; //binary : 0111
-    x=1<<1; //suppose to toggle bit number 1 which makes the number : 0101 means 5
-    result2=result1^x;
-    printf("toggle bit 1 in %d : %d\n",result1,result2);
-    return 0;
+int main() {
+	int number, nth, result, x, op;
+	printf("Enter any number: ");
+	scanf("%d", &number);
+	printf(
+			"Choose your operation by writing the equavilent number \
+    		1(set) 2(clear) 3(toggle)\n");
+	scanf("%d", &op);
+	printf("Enter nth bit to set, clear or toggle : ");
+	scanf("%d", &nth);
+	switch (op) {
+	case 1:
+		x = 1 << nth;
+		result = number | x;
+		printf("set bit %d in %d result : %d\n", nth, number, result);
+		break;
+	case 2:
+		x = ~(1 << nth);
+		result = number & x;
+		printf("clear bit %d in %d result : %d\n", nth, number, result);
+		break;
+	case 3:
+		x = 1 << nth;
+		result = number ^ x;
+		printf("toggle bit %d in %d result : %d\n", nth, number, result);
+		break;
+	default:
+		printf("you choose a wrong number\n");
+		break;
+	}
+
+	return 0;
 }
