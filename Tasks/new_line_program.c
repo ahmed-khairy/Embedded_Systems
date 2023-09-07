1ba55af79db33ec9c332c26cd9a82ced4905dd33
Ahmed khairy <52660420+ahmed-khairy@users.noreply.github.com>
2023-09-07 21:53:18 +0300

    Create new_line_program.c

---
+++ Tasks/new_line_program.c
@@ -0,0 +1,14 @@
+/*
+ *Embedded systems software diploma
+ *Author : Ahmed khairy
+ *Date : 7/9/2023
+ *Program function: program that print your name and your grade in a new line.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	printf("\nHello my name is Ahmed.\nI'm 33 years old.\n");
+	return 0;
+}
