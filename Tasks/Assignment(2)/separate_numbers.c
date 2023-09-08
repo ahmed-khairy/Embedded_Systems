f4d45f56c9525df46896e1848978d5340955df3d
Ahmed khairy <52660420+ahmed-khairy@users.noreply.github.com>
2023-09-09 01:55:47 +0300

    Create separate_numbers.c

---
+++ Tasks/Assignment(2)/separate_numbers.c
@@ -0,0 +1,34 @@
+/*
+*Embedded systems software diploma
+*Author : Ahmed khairy
+*Date : 9/4/2023
+*Session :
+*Program function: convert any decimal number to binary and print it on screen
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int number; //user number
+    unsigned char reminder;   //the binary value bit by bit
+    unsigned char counter=0;  //counter for number of bits
+    int binary[8];  //resulted binary number
+    unsigned char i;  //counter for printing result
+    printf("Enter any decimal number : ");
+    scanf("%d",&number);
+    while(number>=1)
+    {
+        reminder=number%10;
+        number/=10;
+        binary[counter]=reminder;
+        counter++;
+//        printf("%hhu",reminder);
+    }
+    //loop for reorder binary number
+    for(i =counter-1; i>=0; i--)
+        printf("%d  ",binary[i]);
+    printf("\n");
+    return 0;
+}
