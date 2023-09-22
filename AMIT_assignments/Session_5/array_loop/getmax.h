d36e9caa3d2abc0ffb0300df301b31be8d700783
Ahmed khairy <52660420+ahmed-khairy@users.noreply.github.com>
2023-09-22 21:46:51 +0300

    Delete AMIT_assignments/Session_5 directory

--- AMIT_assignments/Session_5/array_loop/getmax.h
+++
@@ -1,6 +0,0 @@
-#ifndef PRINT_ELEMENTS_H_
-#define PRINT_ELEMENTS_H_
-
-void getMax(int arr[], int arr_size);
-
-#endif /* PRINT_ELEMENTS_H_ */
--- AMIT_assignments/Session_5/array_loop/getnumbers.h
+++
@@ -1,8 +0,0 @@
-
-
-#ifndef PRINT_NUMBERS_H_
-#define PRINT_NUMBERS_H_
-
-void getNumbers(int arr[], int arr_size);
-
-#endif /* PRINT_ELEMENTS_H_ */
