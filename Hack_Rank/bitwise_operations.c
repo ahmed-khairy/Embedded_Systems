1cd99d032304c955bb1b3c9700f21d4e4620874c
Ahmed khairy <ahmedkairy@gmail.com>
2023-10-08 18:42:39 +0300

    Update bitwise_operations.c

--- Hack_Rank/bitwise_operations.c
+++ Hack_Rank/bitwise_operations.c
@@ -6,38 +6,28 @@
 
 int andMax,orMax,xorMax,temp;
 void calculate_the_maximum(int n, int k) {
-    for(int i=1;i<=n;i++)
-    {
-        for(int j=i+1;j<=n;j++)
-        {
-            // temp=i & j;
-            // if(temp<k)
-                // andMax+=1;
-            // temp=i | j;
-            // if(temp<k)
-                // orMax+=1;
-            // temp=i ^ j;
-            // if(temp<k)
-                // xorMax+=1;
-            temp=i&j;
-            if(temp<k&&temp>andMax)
-                andMax+=1;
-            temp=i|j;
-            if(temp<k&&temp>orMax)
-                orMax+=1;
-            if(temp<k&&temp>xorMax)
-                xorMax+=1;
-        }
-    }
-    printf("%d\n%d\n%d\n",andMax,orMax,xorMax);
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=i+1;j<=n;j++)
+		{
+			temp=i&j;
+			if(temp<k&&temp>andMax)
+				andMax+=1;
+			temp=i|j;
+			if(temp<k&&temp>orMax)
+				orMax+=1;
+			temp=i^j;
+			if(temp<k&&temp>xorMax)
+				xorMax+=1;
+		}
+	}
+	printf("%d\n%d\n%d\n",andMax,orMax,xorMax+1);
 }
 
 int main() {
     int n, k;
   
     scanf("%d %d", &n, &k);
-    if(n<=1000&&n>=2)
-    if(k<=n&&k>=2)
     calculate_the_maximum(n, k);
  
     return 0;
